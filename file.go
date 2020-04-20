15928b1c2f40aa0c135d5ff14abc588c205f3059
Benjamin Smith <ben.smith.developer@gmail.com>
2020-04-20 15:42:03 -0400

    Update file.go

--- file.go
+++ file.go
@@ -9,6 +9,29 @@ import (
 
 var logs = log.Logger()
 
+// FileExists checks if path exists and is a file
+func FileExists(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err == nil {
+		return !fileInfo.IsDir(), nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, errors.Wrapf(err, "failed to check if file exists %s", path)
+}
+
+// DirExists checks if path exists and is a directory
+func DirExists(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err == nil {
+		return info.IsDir(), nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
 func DoesFileContainString(s string, pathToFile string) (bool, int, error) {
 	replacer := strings.NewReplacer("~", os.Getenv("HOME"))
 	pathToFile = replacer.Replace(pathToFile)
